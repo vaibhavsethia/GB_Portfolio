d7311d74e7b89493779727478c94fb95031f22a5
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-09-08 12:42:43 +0530

    Setup and Approuter setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch } from "react-router-dom";
+import { Layout, Affix } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AppRoute from "./Utils/AppRoute";
+import Home from "./Views/Home";
+
+import "./App.css";
+
+const { Content } = Layout;
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Affix></Affix>
+        <Content>
+          <Switch>
+            <AppRoute exact path="/" component={Home} />
+          </Switch>
+        </Content>
+      </div>
+    );
+  }
 }
 
 export default App;
