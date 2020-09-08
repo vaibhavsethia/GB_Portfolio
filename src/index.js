2ab278eddf1f883fa1185373d4aa661c71234d0a
vaibhavsethia <vaibhavsethia1998@gmail.com>
2020-09-08 12:47:56 +0530

    resources

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createBrowserHistory } from "history";
 
 import App from "./App";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 
 const history = createBrowserHistory();
